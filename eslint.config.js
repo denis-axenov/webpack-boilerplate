2927f8f3e32ce81f9e4601136884961b49bf2128
Denis Axenov <denis_axenov@pm.me>
2025-01-02 19:33:26 +0500

    Migrated configs from CommonJS to ES Module

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-const js = require("@eslint/js");
-const globals = require("globals");
+import js from "@eslint/js";
+import globals from "globals";
 
 
-module.exports = [
-    js.configs.recommended,
+export default [
     {
+        ...js.configs.recommended,
         files: [
             "src/scripts/**/*.js"
         ],
