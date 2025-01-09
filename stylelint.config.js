1947e052f4473e4fc47561ecea8da362df8b1416
Denis Axenov <denis_axenov@pm.me>
2025-01-09 23:36:06 +0500

    Optimized stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,13 +5,39 @@ export default {
     ],
     customSyntax: "postcss-scss",
     rules: {
-        "scss/dollar-variable-pattern": "^[a-z][a-zA-Z0-9]*$",
-        "scss/selector-no-redundant-nesting-selector": true,
-        "annotation-no-unknown": null,
-        "at-rule-no-unknown": null,
-        "comment-no-empty": null,
+        "annotation-no-unknown": [
+            true,
+            {
+                "ignoreAnnotations": [
+                    "default"
+                ]
+            }
+        ],
+        "at-rule-no-unknown": [
+            true,
+            {
+                "ignoreAtRules": [
+                    "mixin",
+                    "use",
+                    "include",
+                    "function",
+                    "for",
+                    "each",
+                    "if",
+                    "else",
+                    "while",
+                    "return",
+                    "at-root",
+                    "extend",
+                    "error",
+                    "warn",
+                    "debug",
+                    "forward"
+                ]
+            }
+        ],
+        "declaration-no-important": true,
         "function-no-unknown": null,
-        "media-query-no-invalid": null,
         "no-invalid-position-at-import-rule": [
             true,
             {
@@ -20,20 +46,6 @@ export default {
                     "forward"
                 ]
             }
-        ],
-        "scss/at-extend-no-missing-placeholder": true,
-        "scss/at-if-no-null": true,
-        "scss/at-rule-no-unknown": true,
-        "scss/comment-no-empty": true,
-        "scss/declaration-nested-properties-no-divided-groups": true,
-        "scss/dollar-variable-no-missing-interpolation": true,
-        "scss/function-quote-no-quoted-strings-inside": true,
-        "scss/function-unquote-no-unquoted-strings-inside": true,
-        "scss/load-no-partial-leading-underscore": true,
-        "scss/no-duplicate-mixins": true,
-        "scss/no-global-function-names": true,
-        "scss/operator-no-newline-after": true,
-        "scss/operator-no-newline-before": true,
-        "scss/operator-no-unspaced": true
+        ]
     }
 };
